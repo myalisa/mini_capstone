7b1614ed52acf5f00af4c042c64c1a51014df47e
myalisa <myalisamiro@gmail.com>
2019-08-29 11:59:46 -0500

    Added Products

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -1,3 +1,6 @@
 class Api::ProductsController < ApplicationController
-  
+  def all_products
+    @products = Product.all
+    render 'product_view.json.jb'
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
     namespace :api do
-      get '/product_1' => 'product#product_1_action'
-      get '/product_2' => 'product#product_2_action'
-      get '/product_3' => 'product#product_3_action'
+      get '/all_products' => 'products#all_products'
     end
+      
 end

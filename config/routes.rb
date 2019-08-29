3b7df323c13cd091976a2d96a340a1f48ed85600
myalisa <myalisamiro@gmail.com>
2019-08-29 11:10:26 -0500

    Commit 2

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,7 @@
 Rails.application.routes.draw do
-  # EXAMPLE HTML ROUTE
-  # get "/photos" => "photos#index"
-
-  # EXAMPLE JSON ROUTE WITH API NAMESPACE
-  # namespace :api do
-  #   get "/photos" => "photos#index"
-  # end
+    namespace :api do
+      get '/product_1' => 'product#product_1_action'
+      get '/product_2' => 'product#product_2_action'
+      get '/product_3' => 'product#product_3_action'
+    end
 end

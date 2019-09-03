496b98bbcd7d8f37695dfe6e45efec19ae28ac4b
myalisa <myalisamiro@gmail.com>
2019-09-03 09:15:21 -0500

    minicapstone_updates

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -1,6 +1,28 @@
 class Api::ProductsController < ApplicationController
-  def all_products
+  def index
     @products = Product.all
-    render 'product_view.json.jb'
+    render 'index.json.jb'
+    
   end
+
+  def create
+    @product = Product.new(
+                          name: params[:name],
+                          price: params[:price],
+                          image_url: params[:image_url],
+                          description: params[:description]
+                          )
+
+    @product.save
+    render 'show.json.jb'
+
+  end
+
+  def show
+    @product = Product.find(params[:id])
+    render 'show.json.jb'
+    
+  end
+ 
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
     namespace :api do
-      get '/all_products' => 'products#all_products'
+     get '/products' => 'products#index'
+     post '/products' => 'products#create'
+     get '/products/:id' => 'products#show'
     end
       
 end

a8a86b6fec6914f308b62a9d5d3cc0d8f93b8236
myalisa <myalisamiro@gmail.com>
2019-09-04 11:33:32 -0500

    Updated Partials

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -1,4 +1,4 @@
-class Api::ProductsController < ApplicationController
+ class Api::ProductsController < ApplicationController
   def index
     @products = Product.all
     render 'index.json.jb'
@@ -21,6 +21,26 @@ class Api::ProductsController < ApplicationController
   def show
     @product = Product.find(params[:id])
     render 'show.json.jb'
+    
+  end
+
+  def update
+    @product = Product.find(params[:id])
+
+    @product.name = params[:name] || @product.name
+    @product.price = params[:price] || @product.price
+    @product.image_url = params[:image_url] || @product.image_url
+    @product.description = params[:description] || @product.description
+
+    @product.save
+    render 'show.json.jb'
+    
+  end
+
+  def destroy
+    @products = Product.find(params[:id])
+    @products.destroy
+    render json: {message: "Successfully destroyed product."}
     
   end
  
